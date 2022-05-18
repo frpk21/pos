e13ec58d335a2a6a0202cb6d2124b1af2ca1272c
frpk21 <48810820+frpk21@users.noreply.github.com>
2022-05-18 09:53:22 -0500

    updte ll

--- facturas/admin.py
+++ facturas/admin.py
@@ -1,6 +1,94 @@
 from django.contrib import admin
-from facturas.models import Facturas, Factp, FormasPagos
+from facturas.models import Facturas, Factp, FormasPagos, PagosCartera, Cierres, Cierres1, GrabadosCierres1, FormasPagosCierres1, Vales, PagosVales
+
+class FacturasAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Facturas
+    
+    def get_queryset(self, request):
+        qs = Facturas.objects.all()
+        return qs.filter(user=request.user)
+
+class FactpAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Factp
+    
+    def get_queryset(self, request):
+        qs = Factp.objects.all()
+        return qs.filter(user=request.user)
+
+class FormasPagosAdmin(admin.ModelAdmin):
+    class Meta:
+        model = FormasPagos
+    
+    def get_queryset(self, request):
+        qs = FormasPagos.objects.all()
+        return qs.filter(user=request.user)
+
+class PagosCarteraAdmin(admin.ModelAdmin):
+    class Meta:
+        model = PagosCartera
+    
+    def get_queryset(self, request):
+        qs = PagosCartera.objects.all()
+        return qs.filter(user=request.user)
+
+class CierresAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Cierres
+    
+    def get_queryset(self, request):
+        qs = Cierres.objects.all()
+        return qs.filter(user=request.user)
+
+class Cierres1Admin(admin.ModelAdmin):
+    class Meta:
+        model = Cierres1
+    
+    def get_queryset(self, request):
+        qs = Cierres1.objects.all()
+        return qs.filter(user=request.user)
+
+class GrabadosCierres1Admin(admin.ModelAdmin):
+    class Meta:
+        model = GrabadosCierres1
+    
+    def get_queryset(self, request):
+        qs = GrabadosCierres1.objects.all()
+        return qs.filter(user=request.user)
+
+class FormasPagosCierres1Admin(admin.ModelAdmin):
+    class Meta:
+        model = FormasPagosCierres1
+    
+    def get_queryset(self, request):
+        qs = FormasPagosCierres1.objects.all()
+        return qs.filter(user=request.user)
+
+class ValesAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Vales
+    
+    def get_queryset(self, request):
+        qs = Vales.objects.all()
+        return qs.filter(user=request.user)
+
+class PagosValesAdmin(admin.ModelAdmin):
+    class Meta:
+        model = PagosVales
+    
+    def get_queryset(self, request):
+        qs = PagosVales.objects.all()
+        return qs.filter(user=request.user)
+
 # Register your models here.
-admin.site.register(Facturas)
-admin.site.register(Factp)
-admin.site.register(FormasPagos)
+admin.site.register(FacturasAdmin, Facturas)
+admin.site.register(FactpAdmin, Factp)
+admin.site.register(FormasPagosAdmin, FormasPagos)
+admin.site.register(PagosCarteraAdmin, PagosCartera)
+admin.site.register(CierresAdmin, Cierres)
+admin.site.register(Cierres1Admin, Cierres1)
+admin.site.register(GrabadosCierres1Admin, GrabadosCierres1)
+admin.site.register(FormasPagosAdmin, FormasPagosCierres1)
+admin.site.register(ValesAdmin, Vales)
+admin.site.register(PagosValesAdmin, PagosVales)
